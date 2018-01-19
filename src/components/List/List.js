9dc74ca73e8b82115d2f9a8b7bf73e9c4a58542c
Sangeet Manghnani <smanghna@in.ibm.com>
2018-01-19 14:53:25 +0530

    theme styles added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, Image, View } from 'react-native';
 import { StyleSheet } from 'react-native';
+import theme from '../common/theme';
 // import styles from './style';
 import { List, ListItem, Card, CardItem, Body, Left, Icon, Right, Content, Row, Col, Grid, Container } from 'native-base';
 
@@ -18,22 +19,22 @@ export default class VehicleList extends React.Component {
       const vehicleList = [];
       this.props.listToRender.map((item, index) => {
         vehicleList.push( 
-            <Card style={styles.cardStyles} key={index}>
+            <Card style={theme.card} key={index}>
                 <CardItem cardBody style={styles.imageContainer}>
                     {this.props.selectedType === 'Bikes' ?
-                        <Image resizeMode="cover" source={{uri: 'http://www.sagmart.com/car-images/royal-enfield-bullet/appearance-bullet.jpg'}} style={styles.imageStyles}/>
+                        <Image resizeMode="cover" source={{uri: 'http://www.sagmart.com/car-images/royal-enfield-bullet/appearance-bullet.jpg'}} style={theme.image}/>
                         :   
-                        <Image resizeMode="cover" source={{uri: 'http://resize.indiatvnews.com/en/resize/newbucket/715_-/2016/07/hoda-activa-1469096847.jpg'}} style={styles.imageStyles}/>
+                        <Image resizeMode="cover" source={{uri: 'http://resize.indiatvnews.com/en/resize/newbucket/715_-/2016/07/hoda-activa-1469096847.jpg'}} style={theme.image}/>
                     }
                 </CardItem>
-                <Text style={styles.textStyles}>{item.maker} {item.name}</Text>
+                <Text style={theme.text_bold}>{item.maker} {item.name}</Text>
                 <CardItem cardBody style={styles.imageContainer}>
                     <Left>
-                        <Text style={styles.contentText}>Available From : </Text>
+                        <Text style={theme.text_normal}>Available From : </Text>
                         <Text>{item.availableFrom}</Text>
                     </Left>
                     <Right>
-                        <Text style={styles.contentText}>@{item.chargesPerKm}/km</Text>
+                        <Text style={theme.text_normal}>@{item.chargesPerKm}/km</Text>
                     </Right>
                 </CardItem>
             </Card>
@@ -50,7 +51,7 @@ export default class VehicleList extends React.Component {
     return (
         <View style={styles.container}>
             <Content style={styles.contentStyle}>
-            <Text style={styles.headerStyle}>{this.props.selectedType}</Text>
+            <Text style={theme.heading}>{this.props.selectedType}</Text>
                 {this.renderList()}
             </Content>
         </View>
@@ -59,33 +60,8 @@ export default class VehicleList extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    headerStyle: {
-        fontFamily: 'Circular-Bold',
-        fontSize: 50,
-        paddingLeft: 15,
-    },
-    cardStyles:{
-        marginBottom: 30,
-        shadowOpacity: 0,
-        borderWidth: 0,
-        borderRadius: 0,
-        padding: 15,
-        borderTopWidth:0,
-        borderBottomWidth:0,
-        borderRightWidth:0,
-        borderLeftWidth:0,
-        shadowOffset: {"width": 0, "height": 0},
-        shadowRadius: 0,
-        elevation: 0
-    },
     imageContainer: {
         marginBottom: 20,
-    },
-    imageStyles:{
-        height: 200, 
-        width: '100%', 
-        borderWidth:0,
-        borderRadius: 2,
     },
     rowStyle:{
         flexDirection:'column',
@@ -94,14 +70,4 @@ const styles = StyleSheet.create({
         borderBottomWidth: 0,
         borderWidth: 0,
       },
-      textStyles: {
-          fontWeight: "800",
-          fontSize:30,
-          fontFamily: 'Circular-Bold',
-      },
-      contentText: {
-          fontSize: 15,
-          fontFamily: 'Circular',
-          fontWeight: "500"
-      }
 });  
