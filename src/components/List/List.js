4c0a4ea2718a257c3164805172489be83d9a2174
Chethan Kumar N. Settipalli <chethankumar.sn@in.ibm.com>
2018-01-21 21:06:10 +0530

    grid or list ui

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Text, Image, View, TouchableOpacity, StyleSheet } from 'react-native';
+import GridView from 'react-native-super-grid';
+import { List, ListItem, Card, CardItem, Body, Left, Icon, Right, Content, Row, Col, Grid, Container } from 'native-base';
 import theme from '../common/theme';
 import styles from './style';
-import { List, ListItem, Card, CardItem, Body, Left, Icon, Right, Content, Row, Col, Grid, Container } from 'native-base';
-
-import BikeDetail from '../BikeDetail/BikeDetail';
 
 const defaultProps = {
   listToRender: [],
@@ -13,8 +12,12 @@ const defaultProps = {
 export default class VehicleList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      interfaceType: 'grid',
+    };
 
     this.renderList = this.renderList.bind(this);
+    this.renderGrid = this.renderGrid.bind(this);
   }
 
   renderList() {
@@ -39,7 +42,7 @@ export default class VehicleList extends React.Component {
             </Right>
           </CardItem>
         </Card>
-                       </TouchableOpacity>);
+      </TouchableOpacity>);
     });
 
     return (
@@ -48,12 +51,72 @@ export default class VehicleList extends React.Component {
       </List>
     );
   }
+
+  renderGrid() {
+    const vehicleList = [];
+    this.props.listToRender.map((item, index) => {
+      vehicleList.push(<TouchableOpacity onPress={() => { this.props.onTap(item); }} key={index}>
+        <Card style={theme.card} key={index}>
+          <CardItem cardBody style={styles.imageContainer}>
+            {this.props.selectedType === 'Bikes' ?
+              <Image resizeMode="cover" source={{ uri: item.imgurl }} style={theme.image} />
+                        :
+              <Image resizeMode="cover" source={{ uri: item.imgurl }} style={theme.image} />
+                    }
+          </CardItem>
+          <Text style={theme.text_bold}>{item.maker} {item.name}</Text>
+          <CardItem cardBody style={styles.imageContainer}>
+            <Left>
+              <Text style={theme.text_normal}>Available From {item.availableFrom}</Text>
+            </Left>
+            <Right>
+              <Text style={theme.text_normal}>@{item.chargesPerKm}/km</Text>
+            </Right>
+          </CardItem>
+        </Card>
+      </TouchableOpacity>);
+    });
+
+    return (
+      <GridView
+        itemDimension={130}
+        style={styles.container}
+        items={this.props.listToRender}
+        renderItem={item => (
+          <TouchableOpacity onPress={() => { this.props.onTap(item); }} >
+            <Card style={theme.card} >
+              <CardItem cardBody style={styles.imageContainer}>
+                {this.props.selectedType === 'Bikes' ?
+                  <Image resizeMode="cover" source={{ uri: item.imgurl }} style={theme.image} />
+                        :
+                  <Image resizeMode="cover" source={{ uri: item.imgurl }} style={theme.image} />
+                    }
+              </CardItem>
+              <Text style={theme.text_bold}>{item.maker} {item.name}</Text>
+              <CardItem cardBody style={styles.imageContainer}>
+                <Left>
+                  <Text style={theme.text_normal}>Available From {item.availableFrom}</Text>
+                </Left>
+                <Right>
+                  <Text style={theme.text_normal}>@{item.chargesPerKm}/km</Text>
+                </Right>
+              </CardItem>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Content style={styles.contentStyle}>
           <Text style={theme.heading}>{this.props.selectedType}</Text>
-          {this.renderList()}
+          {this.state.interfaceType === 'list' ?
+          this.renderList()
+          :
+          this.renderGrid()}
         </Content>
       </View>
     );
