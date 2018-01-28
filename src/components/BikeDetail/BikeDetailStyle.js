8a37dda2ccb317c4c5a1307e8d6b728fe9993cc5
Chethan Kumar N. Settipalli <chethankumar.sn@in.ibm.com>
2018-01-28 15:33:10 +0530

    fixing style

--- src/components/BikeDetail/BikeDetailStyle.js
+++ src/components/BikeDetail/BikeDetailStyle.js
@@ -72,7 +72,8 @@ const styles = StyleSheet.create({
   },
   reviewsCard: {
     padding: 15,
-    borderTopWidth: 0.5,
+    // borderTopWidth: 0.5,
+    backgroundColor: '#fff',
   },
   bookedText: {
     color: '#FF5722',
